1ef3cef4c9907256058a8d52ea15ef562ab61a40
varlakov.alexander <varlakov.alexander@gmail.com>
2012-07-29 22:18:11 +0400

    bycicle

---
+++ src/alexander/bycicle.java
@@ -0,0 +1,36 @@
+package alexander;
+
+class Bicycle {
+
+	int cadence = 0;
+	int speed = 0;
+	int gear = 1;
+
+	Bicycle(int cadence, int gear, int speed) {
+		this.cadence = cadence;
+		this.gear = gear;
+		this.speed = speed;
+	}
+
+	void changeCadence(int newValue) {
+		cadence = newValue;
+	}
+
+	void changeGear(int newValue) {
+		gear = newValue;
+	}
+
+	void speedUp(int increment) {
+		speed += increment;
+	}
+
+	void applyBrakes(int decrement) {
+		speed -= decrement;
+	}
+
+	void printStates() {
+		System.out.println("cadence:" + cadence + " speed:" + speed + " gear:"
+				+ gear);
+	}
+
+}
