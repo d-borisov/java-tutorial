1fd9e15403812ea4def1be82b2f8c3080989739c
Oleg M Prozorov <pro.oleg@gmail.com>
2012-08-27 23:13:54 +0400

    generics

---
+++ src/oleg/Pair.java
@@ -0,0 +1,37 @@
+package oleg;
+
+public class Pair<K, V> {
+
+  private K key;
+  private V value;
+
+  // Generic constructor
+  public Pair(K key, V value) {
+    this.key = key;
+    this.value = value;
+  }
+
+  // Generic methods
+  public void setKey(K key) {
+    this.key = key;
+  }
+
+  public void setValue(V value) {
+    this.value = value;
+  }
+
+  public K getKey() {
+    return key;
+  }
+
+  public V getValue() {
+    return value;
+  }
+}
+
+
+class Util {
+  public static <K, V> boolean compare(Pair<K, V> p1, Pair<K, V> p2) {
+    return p1.getKey().equals(p2.getKey()) && p1.getValue().equals(p2.getValue());
+  }
+}
