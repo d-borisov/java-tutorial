fe92175b65a66ff17838c49893c0128fec36c962
Rock_Jam <5min4eq.unity@gmail.com>
2012-10-12 00:28:24 +0400

    nio tutorial

--- src/nikolai/nio/RealPath.java
+++ src/nikolai/nio/RealPath.java
@@ -1,17 +1,20 @@
 package nikolai.nio;
 
 import java.io.IOException;
-import java.nio.file.CopyOption;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Date;
+
+import static java.lang.String.format;
 
 public class RealPath {
   public static void main(String[] args) throws IOException {
-    Path path = Paths.get("README.md");
+    Path path = Paths.get("xanadu.txt");
     System.out.println("Exists " + Files.exists(path, LinkOption.NOFOLLOW_LINKS));
     System.out.println("isReadable " + Files.isReadable(path));
 
@@ -29,13 +32,14 @@ public class RealPath {
     }
 
     if (Files.deleteIfExists(Paths.get("outagain.txt"))) {
-      System.out.println("deleted!");
+      System.out.println("outagain.txt deleted!");
     }
 
     Path p1 = Paths.get("xanadu.txt");
     Path p2 = Paths.get("outagain.txt");
+    Path p3 = Paths.get("outagain2.txt");
     Files.copy(p1, p2, LinkOption.NOFOLLOW_LINKS);
 
-    Files.move(p1,p2, StandardCopyOption.REPLACE_EXISTING);
+    Files.move(p2, p3, StandardCopyOption.REPLACE_EXISTING);
   }
 }
