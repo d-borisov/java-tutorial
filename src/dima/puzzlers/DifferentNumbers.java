a49c24840dd24774479309aa8f809fada7e5cbff
Дмитрий Борисов <opportune.flander@gmail.com>
2013-02-01 09:21:26 +0400

    1. Numbers presentations 2. TreeSet details

---
+++ src/dima/puzzlers/DifferentNumbers.java
@@ -0,0 +1,11 @@
+package dima.puzzlers;
+
+public class DifferentNumbers {
+
+  public static void main(String[] args) {
+    System.out.println(11);
+    System.out.println(011);
+    System.out.println(0x11);
+    System.out.println(0b11);
+  }
+}
